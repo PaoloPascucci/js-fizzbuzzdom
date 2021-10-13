fd1d9dc13aa66787961ed4a968c788442cb09202
PaoloPascucci <pascuccip9@gmail.com>
2021-10-13 16:38:04 +0200

    Add Ciclo corretto

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,22 @@
-const list = document.querySelector(".list")
+const ul = document.querySelector("ul.list")
 
-for (let i = 0; i <= 100; i++) {
-    const li = `<li class="box box--${i}">${i}</li>`;
-    list.innerHTML += li;
+for (let i = 1; i <= 100; i++) {
 
-    if (i % 3 === 0) {
-        list.toggleAttribute("class", "fizz");
+
+    if (i % 15 === 0) {
+        const li = `<li class="fizzbuzz fizzbuzz--${i}">FizzBuzz</li>`;
+        ul.innerHTML += li;
     }
     else if (i % 5 === 0) {
-        list.toggleAttribute("class", "buzz");
+        const li = `<li class="buzz buzz--${i}">Buzz</li>`;
+        ul.innerHTML += li;
     }
-    else if (i % 15 === 0) {
-        list.toggleAttribute("class", "fizzbuzz");
+    else if (i % 3 === 0) {
+        const li = `<li class="fizz fizz--${i}">Fizz</li>`;
+        ul.innerHTML += li;
+    }
+    else {
+        const li = `<li class="box box--${i}">${i}</li>`;
+        ul.innerHTML += li;
     }
 }
